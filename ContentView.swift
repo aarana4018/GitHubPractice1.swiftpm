69db2c780a93c2a2a65341ff1a3ddd94d03fd86f
Andrea Arana <aarana4018@stu.d214.org>
2023-04-14 12:52:10 -0500

    Variables and TextFields

--- ContentView.swift
+++ ContentView.swift
@@ -1,12 +1,14 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    @State var integer1: Int = 0
+    @State var integer2: Int = 0
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
-        }
+        
+            TextField("Integer One", value: $integer1, format: .number )
+            
+            TextField("Integer Two", value: $integer2, format: .number)
     }
 }
