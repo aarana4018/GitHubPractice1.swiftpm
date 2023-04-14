7e95f82fae18918e24eae6614338f85de9c10394
Anna Wojnicki <awojnicki3464@stu.d214.org>
2023-04-14 12:47:10 -0500

    button and text

--- ContentView.swift
+++ ContentView.swift
@@ -1,12 +1,18 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    @State var answer: String
+    
     var body: some View {
+        
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+           
+            Button("Multiply") {
+                
+            }
+            
+            Text("\(answer)")
         }
     }
 }
